690d3e4efde4985f6978cbbcd4311deaebba2c43
Anthony LoGreco <aclogreco@gmail.com>
2014-09-02 07:19:33 -0400

    Complete exercise 13 extra credit.

--- ex13.c
+++ ex13.c
@@ -1,52 +1,54 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    printf("ERROR: You need one argument.\n");
+  if (argc < 2) {
+    printf("ERROR: You need at least one argument.\n");
     return 1;
   }
   
-  int i = 0;
-  for (i = 0; argv[1][i] != '\0'; i++) {
-    char letter = argv[1][i];
-    
-    switch (letter) {
-    case 'a':
-    case 'A':
-      printf("%d: 'A'\n", i);
-      break;
-      
-    case 'e':
-    case 'E':
-      printf("%d: 'E'\n", i);
-      break;
-      
-    case 'i':
-    case 'I':
-      printf("%d: 'I'\n", i);
-      break;
-      
-    case 'o':
-    case 'O':
-      printf("%d: 'O'\n", i);
-      break;
-      
-    case 'u':
-    case 'U':
-      printf("%d: 'U'\n", i);
-      break;
+  int i, n;
+  char letter;
 
-    case 'y':
-    case 'Y':
-      if (i > 2) {
-	printf("%d: 'Y'\n", i);
-      }
-      break;
+  for (n = 1; n < argc; n++) {
+    for (i = 0, letter = '\0'; argv[n][i] != '\0'; i++) {
+      letter = argv[n][i];
+      letter |= 32;
       
-    default:
-      printf("%d: %c is not a vowel\n", i, letter);
+      switch (letter) {
+      case 'a':
+	printf("%d: 'A'\n", i);
+	break;
+	
+      case 'e':
+	printf("%d: 'E'\n", i);
+	break;
+	
+      case 'i':
+	printf("%d: 'I'\n", i);
+	break;
+      
+      case 'o':
+	printf("%d: 'O'\n", i);
+	break;
+	
+      case 'u':
+	printf("%d: 'U'\n", i);
+	break;
+	
+      case 'y':
+	if (i > 0) {
+	  printf("%d: 'Y'\n", i);
+	}
+	break;
+	
+      default:
+	letter &= ~(32);
+	printf("%d: %c is not a vowel\n", i, letter);
+      }
     }
+
+    printf("\n");
   }
-  
+
   return 0;
 }
