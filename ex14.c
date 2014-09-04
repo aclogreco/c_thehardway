afac946afb644b864bf3d6b53856b1ff06109e8f
Anthony LoGreco <aclogreco@gmail.com>
2014-09-04 05:46:33 -0400

    Complete exercise 14 extra credit.

--- ex14.c
+++ ex14.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 // foward declarations
-int can_print_it(char ch);
-void print_letters(char arg[]);
+//int can_print_it(char ch);
+void print_letters(int len, char arg[]);
 
 void print_arguments(int argc, char *argv[]) {
   int i = 0;
-  
+  int s_len = 0;
+
   for (i = 0; i < argc; i++) {
-    print_letters(argv[i]);
+    s_len = strlen(argv[i]);
+    print_letters(s_len, argv[i]);
   }
 }
 
-void print_letters(char arg[]) {
+void print_letters(int len, char arg[]) {
   int i = 0;
   
-  for (i = 0; arg[i] != '\0'; i++) {
+  //for (i = 0; arg[i] != '\0'; i++) {
+  for (i = 0; i < len; i++) {
     char ch = arg[i];
     
-    if (can_print_it(ch)) {
+    //if (can_print_it(ch)) {
+    if (isprint(ch)) {
       printf("'%c' == %d ", ch, ch);
     }
   }
@@ -27,9 +32,9 @@ void print_letters(char arg[]) {
   printf("\n");
 }
 
-int can_print_it(char ch) {
-  return isalpha(ch) || isblank(ch);
-}
+//int can_print_it(char ch) {
+//  return isalpha(ch) || isblank(ch);
+//}
 
 int main(int argc, char *argv[]) {
   print_arguments(argc, argv);
