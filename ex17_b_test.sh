1a593788398e23159ea1a6a463415ca16d50ad4f
Anthony LoGreco <aclogreco@gmail.com>
2015-08-06 23:17:57 -0400

    Exercise 17 - Add verbose output to the original test script and create a test script for the B version.

--- ex17_b_test.sh
+++ ex17_b_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Exercise 17 - Simple Database - Test script
+# Exercise 17 version B - Simple Database - Test script
 # This script tests the actions of the simple database.
 
 # abort on error
@@ -10,28 +10,28 @@ echo "Time to test!" | fembot3
 
 # create action test
 echo "Create action test."
-./ex17 testdb.dat c
+./ex17_b testdb_b.dat c 10 256
 echo "Create action test complete."
 echo
 
 # set action test
 echo "Set action test."
-./ex17 testdb.dat s 0 Anthony anthony@example.com
-./ex17 testdb.dat s 1 Zed zed@example.com
-./ex17 testdb.dat s 2 Frank frank@example.com
-./ex17 testdb.dat s 3 Joe joe@example.com
-./ex17 testdb.dat s 4 Ashley ashley@example.com
-./ex17 testdb.dat s 5 Zora zora@example.com
-./ex17 testdb.dat s 6 Fawn fawn@example.com
-./ex17 testdb.dat s 7 Julie julie@example.com
-./ex17 testdb.dat s 8 Melinda mindy@example.com
-./ex17 testdb.dat s 9 Samantha sam@example.com
+./ex17_b testdb.dat s 0 Anthony anthony@example.com
+./ex17_b testdb.dat s 1 Zed zed@example.com
+./ex17_b testdb.dat s 2 Frank frank@example.com
+./ex17_b testdb.dat s 3 Joe joe@example.com
+./ex17_b testdb.dat s 4 Ashley ashley@example.com
+./ex17_b testdb.dat s 5 Zora zora@example.com
+./ex17_b testdb.dat s 6 Fawn fawn@example.com
+./ex17_b testdb.dat s 7 Julie julie@example.com
+./ex17_b testdb.dat s 8 Melinda mindy@example.com
+./ex17_b testdb.dat s 9 Samantha sam@example.com
 echo "Set action test complete."
 echo
 
 # list action test
 echo "List action test."
-./ex17 testdb.dat l
+./ex17_b testdb.dat l
 echo "List action test complete."
 echo
 
@@ -40,7 +40,7 @@ echo
 echo "Get action test."
 ID=0
 while [ $ID -lt 10 ]; do
-    ./ex17 testdb.dat g $ID
+    ./ex17_b testdb.dat g $ID
     let ID=ID+2
 done
 echo "Get action test complete."
@@ -48,13 +48,13 @@ echo
 
 # delete action test
 echo "Delete action test."
-./ex17 testdb.dat d 3
+./ex17_b testdb.dat d 3
 echo "Delete action test complete."
 echo
 
 # list after delete
 echo "Final list."
-./ex17 testdb.dat l
+./ex17_b testdb.dat l
 echo
 
 echo "Testing complete." | fembot3
